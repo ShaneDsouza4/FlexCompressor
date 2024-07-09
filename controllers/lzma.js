bc86c1af8500ff907fdd52272d326fd871ecbadc
ShaneDsouza4 <shane.json96@gmail.com>
2024-07-09 19:37:23 -0400

    LZMA Single Compress & Decompress

---
+++ controllers/lzma.js
@@ -0,0 +1,44 @@
+const TICKET = require("../models/tickets");
+const lzma = require('lzma');
+
+async function handleCreateLZMATicket(req, res){
+    try{
+        lzma.compress(JSON.stringify(req.body), 1, async (result, error) => {
+            if (error) {
+              return res.status(500).json({ msg: "error compressing data", error });
+            }
+            const compressedData = Buffer.from(result);
+            await TICKET.create({
+              data: compressedData
+            });
+            return res.status(201).json({ msg: "success" });
+        });
+    }catch(error){
+        return res.status(500).json({msg:"error creating ticket", error});
+    }
+}
+
+async function handleGetLZMATicketById(req, res) {
+    try {
+      const ticket = await TICKET.findById(req.params.id);
+      if (ticket) {
+        lzma.decompress(ticket.data, (result, error) => {
+          if (error) {
+            return res.status(500).json({ msg: "error decompressing data", error });
+          }
+          const decompressedData = JSON.parse(result.toString());
+          res.status(200).json(decompressedData);
+        });
+      } else {
+        res.status(404).json({ msg: 'Ticket not found' });
+      }
+    } catch (error) {
+      res.status(500).json({ msg: 'Error retrieving ticket', error });
+    }
+  };
+
+
+module.exports = {
+    handleCreateLZMATicket,
+    handleGetLZMATicketById
+}
--- routes/tickets.js
+++ routes/tickets.js
@@ -4,9 +4,17 @@ const {
     handleGetTicketById
 } = require("../controllers/tickets")
 
+const { 
+    handleCreateLZMATicket,
+    handleGetLZMATicketById
+} = require("../controllers/lzma");
+
 const router = express.Router();
 
 router.post("/createTicket", handleCreateTicket);
 router.get("/:id", handleGetTicketById);
+
+router.post("/createLZMATicket", handleCreateLZMATicket);
+router.get("/getLZMA/:id", handleGetLZMATicketById);
 
 module.exports = router;
