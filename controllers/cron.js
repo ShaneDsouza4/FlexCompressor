88c699e8bb21c877d45eb8b41b486fcea1d8dd9e
sheldon124 <sheldoncorrea98@gmail.com>
2024-07-22 11:17:20 -0400

    added function to archive old tickets

--- controllers/cron.js
+++ controllers/cron.js
@@ -1,11 +1,13 @@
 const cron = require("node-cron");
 const {recompressArchived} = require('./archivedtickets');
+const {archiveOldTickets} = require('./tickets');
 
 function runCron() {
   // cron to run task
   cron.schedule("* * * * *", async () => {
     console.log('running a task every minute');
     await recompressArchived();
+    await archiveOldTickets();
   });
 }
 
