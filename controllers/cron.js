f6adf9b1abba8c68f000c8ab244b9a4d01a772a1
ShaneDsouza4 <shane.json96@gmail.com>
2024-07-28 17:04:24 -0400

    Comment Added in Cron

--- controllers/cron.js
+++ controllers/cron.js
@@ -5,8 +5,7 @@ const {archiveOldTickets} = require('./tickets');
 function runCron() {
   //cron.schedule("0 0 * * 0", async () => {// Runs every Sunday 12am
   //cron.schedule("0 0 1 * *", async () => { //Runs at 12am on the first on every month
-  cron.schedule("* * * * *", async () => {
-    console.log('running a task every minute');
+  cron.schedule("* * * * *", async () => { //Runs every minute
     await recompressArchived();
     await archiveOldTickets();
   });
